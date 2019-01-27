2531a5d7dd8418c11084bb9a72881d61c4bf98ff
mac-xu <1027639251@qq.com>
2019-01-27 23:03:47 +0800

    1

--- src/config.js
+++ src/config.js
@@ -14,9 +14,3 @@ require.config({
     }
 });
 
-
-require(['model/contact', 'collection/contactList', 'view/contactView', 'view/appView'], function (model, collection, view, app) {
-
-    var app = new app();
-
-})
---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+require(['model/contact', 'collection/contactList', 'view/contactView', 'view/appView'], function (model, collection, view, app) {
+
+    var app = new app();
+
+})
